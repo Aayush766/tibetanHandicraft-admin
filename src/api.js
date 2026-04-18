67740bc8ebbda8ec466ea0282c010387a2f369ea
Aayush766 <blaze.aayush23@gmail.com>
2026-04-18 21:18:38 +0530

    tokens

--- src/api.js
+++ src/api.js
@@ -2,5 +2,13 @@ import axios from "axios";
 
 export const API = axios.create({
   baseURL: "https://thj-backend.onrender.com/api/hero",
-  withCredentials: true
+});
+
+// Attach admin token to every request automatically
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("admin_token");
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
 });
